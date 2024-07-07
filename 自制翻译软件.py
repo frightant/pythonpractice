70a34aab7150dabffd1f47e11b8fe38b7fbae817
frightant Yu <1258577371@qq.com>
2024-07-07 22:36:33 +0800

    此次更新了一个自制的没有交互页面的翻译程序，需要配合temp.js来使用

---
+++ 自制翻译软件.py
@@ -0,0 +1,36 @@
+import time
+import requests
+import execjs
+import ttkbootstrap as ttk
+
+
+def sign(word):
+    with open('temp.js',encoding='utf-8') as f:
+        text=f.read()
+    jscode = execjs.compile(text)
+    result = jscode.call('baidu',word)
+    return result
+def search(word):
+    result = sign(word)
+    data={
+        'from':'zh',
+        'to':'en',
+        'query':word,
+        'transtype':'translang',
+        'simple_means_flag':'3',
+        'sign':result,
+        'token':'1bf0ec42272ee53aa3334a63fe665037',
+        'domain':'common',
+        'ts':f'{time.time()}'
+    }
+    url = "https://fanyi.baidu.com/v2transapi?from=zh&to=en"
+    headers={
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0',
+        'Cookie':'BAIDUID=2E018F48EBEE468BF0063D7AEAB47EA8:FG=1; BAIDUID_BFESS=2E018F48EBEE468BF0063D7AEAB47EA8:FG=1; BIDUPSID=2E018F48EBEE468BF0063D7AEAB47EA8; PSTM=1712551351; ZFY=aT3Hit2nhSqhrSmrv9yQ2c29ndL1vIaDx23wAHiWUDE:C; H_PS_PSSID=40304_40378_40416_40445_40511_60041_60026_60031_40080_60126; __bid_n=18ef6d3065f92964f38b97; BDUSS=lBNlRtQjdOcVR2dWN2Z1huWXV-ZEJQWFpPQ0U2OGtKQlRHa3dBVFRmTGpub0ptSVFBQUFBJCQAAAAAAAAAAAEAAABRwYKt0-DJ8bTyvNwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOMRW2bjEVtmOG; BDUSS_BFESS=lBNlRtQjdOcVR2dWN2Z1huWXV-ZEJQWFpPQ0U2OGtKQlRHa3dBVFRmTGpub0ptSVFBQUFBJCQAAAAAAAAAAAEAAABRwYKt0-DJ8bTyvNwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOMRW2bjEVtmOG; smallFlowVersion=old; RT="z=1&dm=baidu.com&si=c2ade42c-f3e0-4c71-aea9-4320f0fd6f66&ss=lybctoxm&sl=1&tt=2qb&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=3id&ul=9p2&hd=a3u"; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1720344728; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1720358084; ab_sr=1.0.1_MWI2ZTUxY2E4MTBlODNjZjU0YmVhMDcwNzQ5MTQyZDRmYzJjMTdjMzZiNTUxN2JkNTljMjE5YzZmMmM1YmE2M2Y2Njk5YTJlMTgwNjg5MzIwZjkyMjJjYmNlZWQ5YTk0Njc4MzdlOTFjOTU2OGMxZjY2MGQzMzI0Y2MxZjU2ZTUyZTIxMWI2ZDNkNjk4ZWE0OTFkNTQ3NzMxOWEyNWQwZTk0YjRiNjUyYzUxYTc1MzEwNTQ3ODFkYTAyZGQzZjVh',
+        'Host':'fanyi.baidu.com',
+        'Referer':'https://fanyi.baidu.com/'
+    }
+    html = requests.post(url=url,headers=headers,data=data).json()
+    trans_result = html['trans_result']['data'][0]['dst']
+    print('翻译的结果是:'+trans_result)
+search("我是你爹")
